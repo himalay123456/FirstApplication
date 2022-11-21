5ba2aaa86726bffa1e6a15f3cc241f00037fa283
himalay123456 <himalay@solulab.com>
2022-11-21 13:48:47 +0530

    Auth Screens added

---
+++ src/custom/Field.js
@@ -0,0 +1,23 @@
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable react/self-closing-comp */
+import React from 'react';
+import {TextInput} from 'react-native';
+import {darkGreen} from '../constants/color';
+
+const Field = props => {
+  return (
+    <TextInput
+      {...props}
+      style={{
+        borderRadius: 100,
+        color: darkGreen,
+        paddingHorizontal: 10,
+        width: '78%',
+        backgroundColor: 'rgb(220,220, 220)',
+        marginVertical: 10,
+      }}
+      placeholderTextColor={darkGreen}></TextInput>
+  );
+};
+
+export default Field;
--- src/screens/auth/MainScreen.js
+++ src/screens/auth/MainScreen.js
@@ -1,12 +1,29 @@
-import {View, Text} from 'react-native';
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
+import {View, Text} from 'react-native';
+import {Background, Button} from '../../custom';
+import {darkGreen, green} from '../../constants/color';
 
-const MainScreen = () => {
+const Home = props => {
   return (
-    <View>
-      <Text>MainScreen</Text>
-    </View>
+    <Background>
+      <View style={{position: 'absolute', top: 200, left: 20}}>
+        <Text style={{color: 'white', fontSize: 64}}>Let's Crack</Text>
+        <Button
+          bgColor={green}
+          textColor="white"
+          btnLabel="Login"
+          Press={() => props.navigation.navigate('Login')}
+        />
+        <Button
+          bgColor="white"
+          textColor={darkGreen}
+          btnLabel="Signup"
+          Press={() => props.navigation.navigate('Signup')}
+        />
+      </View>
+    </Background>
   );
 };
 
-export default MainScreen;
+export default Home;
